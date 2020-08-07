53d8f52ec705f8f4509b6ac6c8bde247d5de7170
Mike Sapienza <mikesapaudio@gmail.com>
2020-08-07 19:31:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ def get_japanese_emoticon (file, emo)
         end
       end
       if !japanese
-         "There is no emoticon with those values!"
+         "Sorry, that emoticon was not found"
        else
          japanese
       end
