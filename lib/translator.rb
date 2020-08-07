4ccac279e989bd3b747c5d9bb31745d43934dc68
Mike Sapienza <mikesapaudio@gmail.com>
2020-08-07 19:01:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,6 +17,7 @@ end
 def get_japanese_emoticon (file, emo)
     # code goes here
     load_library(file)
+    results
     #binding.pry
     if emo = results[key][:english]
       results[key][:japanese]
